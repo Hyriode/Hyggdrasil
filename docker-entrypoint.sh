1513d3c225e555ec06b7eaa9334502391cc6e54c
AstFaster <astfaster@gmail.com>
2023-01-02 14:58:31 +0100

    Fix missing '.jar'

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@
 : "${MIN_MEMORY:=256M}}"
 : "${MAX_MEMORY:=4G}}"
 
-echo "[init] Copying HyriSpigot jar"
+echo "[init] Copying Hyggdrasil jar"
 cp /usr/app/Hyggdrasil.jar /hyggdrasil
 
 echo "[init] Starting process..."
-java ${JVM_XX_OPTS} -Xms${MIN_MEMORY} -Xmx${MAX_MEMORY} -jar Hyggdrasil
+java ${JVM_XX_OPTS} -Xms${MIN_MEMORY} -Xmx${MAX_MEMORY} -jar Hyggdrasil.jar
